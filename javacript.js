5dfa2168e4f1dea031352f776d872fc019fd958c
vivek <vivek20Eng@gmail.com>
2021-06-16 14:04:18 +0530

    sampleevent

---
+++ javacript.js
@@ -0,0 +1,18 @@
+let btn=document.getElementById("btn");
+
+    addEventListener("click",function(){
+   
+
+        alert("clicked");
+ 
+    });
+
+
+/*function onalert(){
+    alert("u clicked");
+}
+onalert();*/
+//or............or.......
+/*function myfun(){
+    alert("clicked");
+} */
